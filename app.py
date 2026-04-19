9c6253ecd6f457f32c743028a9a1b41ac8b7d849
haoohaoo <haoohaoohaha@163.com>
2026-04-19 16:12:35 +0800

    feat: ✨输出

--- app.py
+++ app.py
@@ -42,5 +42,5 @@ def health():
     return {"status": "ok"}
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5001))
     app.run(host='0.0.0.0', port=port)
